9daa85a4bc2d786c3909cd4c8e5743bd8195115e
Shubham <Shubham20015>
2022-08-01 01:49:36 +0530

    Integration of supabase

--- pages/investorform/details.js
+++ pages/investorform/details.js
@@ -1,14 +1,17 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useState, useEffect, useRef } from "react";
+import { supabase } from "../../utils/supabaseClient";
 import { useRouter } from "next/router";
 import { Icon } from "@iconify/react";
 import Head from "next/head";
+import { Notify } from "notiflix";
 
 export default function funding() {
+  const submitRef = useRef();
+  const [isCompany, setIsCompany] = useState(false);
   const [inputList, setInputList] = useState([
     {
       companyName: "",
-      dateOfFunding: "",
+      dateOfInvestment: "",
       amountInvested: null,
       equityOwned: null,
       investorPhotoUrl: "",
@@ -28,9 +31,44 @@ export default function funding() {
 
   const router = useRouter();
 
-  const handleSubmit = (data) => {
-    // localStorage.setItem("investorData", JSON.stringify(inputList));
-    router.push("/investor");
+  useEffect(() => {
+    const investorData = JSON.parse(localStorage.getItem("investorData"));
+    if (!investorData) router.push("/investorform");
+    if (investorData && investorData.userType === "company") setIsCompany(true);
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const investorData = JSON.parse(localStorage.getItem("investorData"));
+    investorData["isCompany"] = isCompany;
+    delete investorData.userType;
+
+    try {
+      const { data, error } = await supabase
+        .from("Users")
+        .insert([{ ...investorData }]);
+      if (error) throw new Error("Something went wrong");
+      if (isCompany) {
+        memberList = memberList.map((item) => {
+          return { ...item, user_id: data[0].id };
+        });
+        const { teamData, teamError } = await supabase
+          .from("Team_members")
+          .insert(memberList);
+        if (teamError) throw new Error("Something went wrong with team");
+      }
+      inputList = inputList.map((item) => {
+        return { ...item, user_id: data[0].id };
+      });
+      const { investData, investError } = await supabase
+        .from("Investments")
+        .insert(inputList);
+      if (investError) throw new Error("Something went wrong with investments");
+      // console.log("Success", data);
+      // router.push("/investor");
+    } catch (error) {
+      Notify.failure(error.message, { position: "top-right" });
+    }
   };
 
   const handleInputChange = (e, index) => {
@@ -48,7 +86,10 @@ export default function funding() {
   };
 
   const handleRemoveClick = (index) => {
-    if (window.confirm("Are you sure you want to delete this?")) {
+    let ans = window.confirm(
+      "Are you sure you want to delete this investment?"
+    );
+    if (ans) {
       const list = [...inputList];
       list.splice(index, 1);
       setInputList(list);
@@ -56,7 +97,8 @@ export default function funding() {
   };
 
   const handleTeamRemoveClick = (index) => {
-    if (window.confirm("Are you sure you want to delete this?")) {
+    let ans = window.confirm("Are you sure you want to remove this member?");
+    if (ans) {
       const list = [...memberList];
       list.splice(index, 1);
       setMemberList(list);
@@ -108,10 +150,7 @@ export default function funding() {
                 Add new Investment
               </button>
             </div>
-            <form
-              className="flex flex-col px-10"
-              onSubmit={() => handleSubmit()}
-            >
+            <form className="flex flex-col px-10" onSubmit={handleSubmit}>
               {inputList.map((x, i) => {
                 return (
                   <div className="flex flex-col" key={i}>
@@ -208,134 +247,161 @@ export default function funding() {
                   </div>
                 );
               })}
-            </form>
-            <div className="divider mt-0"></div>
-            <div className="flex justify-between items-center">
-              <p className="text-2xl font-medium">Team Members:</p>
-              <button className="btn btn-primary" onClick={handleTeamAddClick}>
-                <Icon
-                  icon="ant-design:plus-outlined"
-                  className="text-2xl mr-2"
-                />{" "}
-                Add new Member
-              </button>
-            </div>
-            <form
-              className="flex flex-col px-10"
-              onSubmit={() => handleSubmit()}
-            >
-              {memberList.map((x, index) => {
-                return (
-                  <div className="flex flex-col" key={index}>
-                    <p className="text-lg font-medium">Team {index + 1}</p>
-                    <div className="lg:grid grid-cols-3 gap-5 py-2 w-full">
-                      <div className="form-control w-full max-w-xs">
-                        <label className="label">
-                          <span className="label-text">Person Name</span>
-                        </label>
-                        <input
-                          type="text"
-                          name="fullName"
-                          onChange={(e) => handleTeamInputChange(e, index)}
-                          value={x.fullName}
-                          placeholder="Enter full name"
-                          className="input input-bordered w-full max-w-xs"
-                          required
-                        />
-                      </div>
-                      <div className="form-control w-full max-w-xs">
-                        <label className="label">
-                          <span className="label-text">Designation</span>
-                        </label>
-                        <input
-                          type="text"
-                          name="designation"
-                          onChange={(e) => handleTeamInputChange(e, index)}
-                          value={x.designation}
-                          placeholder="Enter Designation"
-                          className="input input-bordered w-full max-w-xs"
-                          required
-                        />
-                      </div>
-                      <div className="form-control w-full max-w-xs">
-                        <label className="label">
-                          <span className="label-text">Member Photo URL</span>
-                        </label>
-                        <input
-                          type="url"
-                          name="memberPhotoUrl"
-                          onChange={(e) => handleTeamInputChange(e, index)}
-                          value={x.memberPhotoUrl}
-                          placeholder="www.example.com"
-                          className="input input-bordered w-full "
-                          required
-                        />
-                      </div>
-                      <div className="form-control w-full max-w-xs">
-                        <label className="label">
-                          <span className="label-text">Linkedin username</span>
-                        </label>
-                        <input
-                          type="url"
-                          name="linkedin"
-                          onChange={(e) => handleTeamInputChange(e, index)}
-                          value={x.linkedin}
-                          placeholder="Type here"
-                          className="input input-bordered w-full"
-                        />
-                      </div>
-                      <div className="form-control w-full max-w-xs">
-                        <label className="label">
-                          <span className="label-text">Twitter username</span>
-                        </label>
-                        <input
-                          type="url"
-                          name="twitter"
-                          onChange={(e) => handleTeamInputChange(e, index)}
-                          value={x.twitter}
-                          placeholder="Type here"
-                          className="input input-bordered w-full"
-                        />
-                      </div>
-                      <div></div>
-                      <div className="form-control col-span-3 w-full mx-auto">
-                        <label className="label">
-                          <span className="label-text">
-                            Small Description here
-                          </span>
-                        </label>
-                        <textarea
-                          name="description"
-                          onChange={(e) => handleTeamInputChange(e, index)}
-                          value={x.description}
-                          className="textarea h-16 resize-none"
-                          minLength={40}
-                          maxLength={120}
-                          placeholder="Write description..."
-                        ></textarea>
-                      </div>
-                      <div>
-                        {memberList.length !== 1 && (
-                          <button
-                            className="flex justify-center items-center text-sm font-medium text-[#F38585] hover:bg-[#F38585] hover:text-slate-700 p-4 rounded-full"
-                            onClick={() => handleTeamRemoveClick(index)}
-                          >
-                            Remove Member
-                          </button>
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </form>
-            <div className="flex justify-end">
-                <input
+              <div className="flex justify-end">
+                <button
                   type="submit"
-                  value="Submit"
-                  className="btn btn-primary w-max px-10"
-                />
-            </div>
+                  ref={submitRef}
+                  className={`btn btn-primary w-max px-10 ${
+                    isCompany ? "hidden" : "block"
+                  }`}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+            {isCompany && (
+              <>
+                <div className="divider mt-0"></div>
+                <div className="flex justify-between items-center">
+                  <p className="text-2xl font-medium">Team Members:</p>
+                  <button
+                    className="btn btn-primary"
+                    onClick={handleTeamAddClick}
+                  >
+                    <Icon
+                      icon="ant-design:plus-outlined"
+                      className="text-2xl mr-2"
+                    />{" "}
+                    Add new Member
+                  </button>
+                </div>
+                <form
+                  className="flex flex-col px-10"
+                  onSubmit={(e) => e.preventDefault()}
+                >
+                  {memberList.map((x, index) => {
+                    return (
+                      <div className="flex flex-col" key={index}>
+                        <p className="text-lg font-medium">Team {index + 1}</p>
+                        <div className="lg:grid grid-cols-3 gap-5 py-2 w-full">
+                          <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                              <span className="label-text">Person Name</span>
+                            </label>
+                            <input
+                              type="text"
+                              name="fullName"
+                              onChange={(e) => handleTeamInputChange(e, index)}
+                              value={x.fullName}
+                              placeholder="Enter full name"
+                              className="input input-bordered w-full max-w-xs"
+                              required
+                            />
+                          </div>
+                          <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                              <span className="label-text">Designation</span>
+                            </label>
+                            <input
+                              type="text"
+                              name="designation"
+                              onChange={(e) => handleTeamInputChange(e, index)}
+                              value={x.designation}
+                              placeholder="Enter Designation"
+                              className="input input-bordered w-full max-w-xs"
+                              required
+                            />
+                          </div>
+                          <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                              <span className="label-text">
+                                Member Photo URL
+                              </span>
+                            </label>
+                            <input
+                              type="url"
+                              name="memberPhotoUrl"
+                              onChange={(e) => handleTeamInputChange(e, index)}
+                              value={x.memberPhotoUrl}
+                              placeholder="www.example.com"
+                              className="input input-bordered w-full "
+                              required
+                            />
+                          </div>
+                          <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                              <span className="label-text">
+                                Linkedin username
+                              </span>
+                            </label>
+                            <input
+                              type="text"
+                              name="linkedin"
+                              onChange={(e) => handleTeamInputChange(e, index)}
+                              value={x.linkedin}
+                              placeholder="Type here"
+                              className="input input-bordered w-full"
+                            />
+                          </div>
+                          <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                              <span className="label-text">
+                                Twitter username
+                              </span>
+                            </label>
+                            <input
+                              type="text"
+                              name="twitter"
+                              onChange={(e) => handleTeamInputChange(e, index)}
+                              value={x.twitter}
+                              placeholder="Type here"
+                              className="input input-bordered w-full"
+                            />
+                          </div>
+                          <div></div>
+                          <div className="form-control col-span-3 w-full mx-auto">
+                            <label className="label">
+                              <span className="label-text">
+                                Small Description here
+                              </span>
+                            </label>
+                            <textarea
+                              name="description"
+                              onChange={(e) => handleTeamInputChange(e, index)}
+                              value={x.description}
+                              className="textarea h-16 resize-none"
+                              minLength={40}
+                              maxLength={120}
+                              placeholder="Write description..."
+                              required
+                            ></textarea>
+                          </div>
+                          <div>
+                            {memberList.length !== 1 && (
+                              <button
+                                className="flex justify-center items-center text-sm font-medium text-[#F38585] hover:bg-[#F38585] hover:text-slate-700 p-4 rounded-full"
+                                onClick={() => handleTeamRemoveClick(index)}
+                              >
+                                Remove Member
+                              </button>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                  <div className="flex justify-end">
+                    <button
+                      type="submit"
+                      onClick={() => submitRef.current.click()}
+                      className="btn btn-primary w-max px-10"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </>
+            )}
           </div>
         </div>
       </div>
